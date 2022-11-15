d47c4a8ac9fb89363dc9a20158b35cc19ab70927
ScienceKidKev <113442292+ScienceKidKev@users.noreply.github.com>
2022-11-15 10:34:32 -0500

    loops

---
+++ loops.py
@@ -0,0 +1,23 @@
+# x=""
+
+
+# while x !="stop":
+#     print("hello world")
+#     x=input('type stop to exit ')
+
+
+# y=0
+
+# while y <10:
+#     y=y+1
+#     print(y)
+
+
+# weathers=["rain","hot","cold"]
+
+# for z in weathers:
+#     print(z)
+
+    
+for i in range(1,11):
+    print(i)
