9accee8ff6fb9d9ba2b6c19819a42715f584b8e8
ScienceKidKev <113442292+ScienceKidKev@users.noreply.github.com>
2022-11-15 09:32:49 -0500

    added list and tuple

---
+++ lists.py
@@ -0,0 +1,28 @@
+#created list#
+my_list=[
+    "dog",
+    "cat",
+    "lion",
+    "rat",
+    "bunny"
+]
+
+print(my_list)
+
+my_list.append('giraffe')
+
+print(my_list[-1])
+
+#created tuple#
+my_tuple=(
+    "hi",
+    "bye",
+    "hola",
+    "wagwan",
+    "yo"
+)
+
+print(my_tuple)
+
+
+
