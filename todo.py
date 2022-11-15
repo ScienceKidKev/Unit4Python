6861425f4f78e8249b52e6595cbe48afbf4fce8e
ScienceKidKev <113442292+ScienceKidKev@users.noreply.github.com>
2022-11-15 10:53:55 -0500

    todo

---
+++ todo.py
@@ -0,0 +1,13 @@
+todos=[]
+x=""
+while x !="stop":
+    x=input("what to add to list?")
+    
+    print('your todo list is')
+    print(todos)
+
+todos.append(x)
+
+
+
+x=input('type stop to exit ')
