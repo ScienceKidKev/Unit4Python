c1cebd7331a724728d91484e3587d9ad44621bb6
ScienceKidKev <113442292+ScienceKidKev@users.noreply.github.com>
2022-11-16 09:26:13 -0500

    len, reverse,join

--- lists.py
+++ lists.py
@@ -25,4 +25,20 @@ my_tuple=(
 print(my_tuple)
 
 
+print(len(my_list))
 
+print(my_list)
+my_list.reverse()
+
+print(my_list)
+
+second_list=[
+    "hat",
+    "beanie",
+    "cap",
+    "hoodie"
+]
+
+print(my_list+second_list)
+
+print(" | ".join(my_list+second_list))
